32f82d90c19deac45fb5c4b488f03e0ed8f2cf87
cvcv88 <cvcv_cv@naver.com>
2024-03-06 17:59:38 +0900

    feat : 플레이어 움직이기, 맵 테두리 만들기

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,8 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    
+    [SerializeField] Rigidbody2D rigid;
+    [SerializeField] SpriteRenderer render;
+    [SerializeField] Animator animator;
+
+    [SerializeField] float movePower;
+    private Vector2 moveDir;
+
+	private void FixedUpdate()
+	{
+		Move();
+	}
+
+	private void Update()
+	{
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+			Debug.Log("UpArrow down");
+			animator.Play("BackWalk");
+        }
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			// nowMode = downAnime;
+			animator.Play("FrontWalk");
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			// nowMode = rightAnime;
+			animator.Play("RightWalk");
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			// nowMode = leftAnime;
+			animator.Play("LeftWalk");
+		}
+	}
+
+	private void Move()
+    {
+        transform.position += new Vector3(moveDir.x * movePower, moveDir.y * movePower, 0) * Time.deltaTime;
+        // rigid.AddForce(Vector2.right * moveDir.x);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveDir = value.Get<Vector2>();
+    }
 }
